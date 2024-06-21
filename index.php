47c74e8827f579a70081eb3f28a3c04bee0bebd2
kapil tamang <kapiltamang123@gmail.com>
2024-06-21 21:05:25 +0545

    fix all pages

--- index.php
+++ index.php
@@ -3,12 +3,6 @@
     require 'helpers/init_conn_db.php';                      
 	?> 	
 <style>
-/*--
-	Author: W3layouts
-	Author URL: http://w3layouts.com
-	License: Creative Commons Attribution 3.0 Unported
-	License URL: http://creativecommons.org/licenses/by/3.0/
---*/
 footer {
   /* position: absolute; */
   bottom: 0;
@@ -20,14 +14,14 @@ form.logout_form {
 	padding: 10px !important;
 }
 body {
-	/* background:url('assets/images/bg2.jpg') no-repeat 0px 0px;
+	background:url('assets/images/Flight.jpg') no-repeat 0px 0px;
 	background-size: cover;
 	font-family: 'Open Sans', sans-serif;
 	background-attachment: fixed;
     background-position: center; */
-	/**background: #bdc3c7;  /* fallback for old browsers */
-/*background: -webkit-linear-gradient(to right, #2c3e50, #bdc3c7);  /* Chrome 10-25, Safari 5.1-6 */
-/*background: linear-gradient(to right, #2c3e50, #bdc3c7); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+	
+background: -webkit-linear-gradient(to right, #2c3e50, #bdc3c7);  /* Chrome 10-25, Safari 5.1-6 */
+
 }
 
 h1,h2,h3,h4,h5,h6{
